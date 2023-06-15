7354f1fcc345965cb3b93051895d2b330718a41e
thfavre <thomas.favre22@yahoo.fr>
2023-06-15 15:51:12 +0200

    first part done

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -7,27 +7,25 @@
 #include "vector2.h"
 
 
-char **create_map(int fd, t_vector2 map_size);
+char		**parse_map(char *filename, t_vector2 map_size);
+t_vector2	get_map_size(char *filename);
 
 bool	parser(char *filename, t_data *data)
 {
-	if (!set_map_size(filename, &data->map_size))
+
+	if (!parse_textures_and_colors())
+		return (false);
+	data->map_size = get_map_size(filename);
+	if (data->map_size.x == 0 || data->map_size.y == 0) // TODO say map size is invalid
 		return (false);
 
-
-	int fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error, '%s' is not a valid file\n", filename);
-		return (false);
-	}
-	data->map = create_map(fd, data->map_size);
-	close(fd);
+	data->map = parse_map(filename, data->map_size);
 	if (data->map == NULL)
 		return (false);
 
 	// prints the map
 	int i = 0;
+	printf("map:\n");
 	while (data->map[i])
 	{
 			printf("%s\n", data->map[i]);
--- srcs/update.c
+++ srcs/update.c
@@ -10,6 +10,21 @@ int on_update(t_data *data)
 {
 	draw_background(&data->img, COLOR_GOLD3);
 
+	int y = 0;
+	int x;
+	int BLOCK_SIZE = 40;
+	while (data->map[y])
+	{
+		x = 0;
+		while (data->map[y][x])
+		{
+			if (data->map[y][x] == '1')
+				draw_rect(&data->img, (t_rect){x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE}, COLOR_BLACK);
+			x++;
+		}
+		y++;
+	}
+
 	get_avrage_fps(data->dt);
 
 	on_update_utils(data);
