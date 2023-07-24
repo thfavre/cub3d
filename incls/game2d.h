a386ff685fd346f3c3bbfbc7145d2821d0b2344e
TastyBao <erard.bastien@gmail.com>
2023-07-24 16:43:15 +0200

    Norminette

--- incls/game2d.h
+++ incls/game2d.h
@@ -1,21 +1,30 @@
 #ifndef  GAME2D_H
 # define GAME2D_H
 
-#include "data.h"
-#include "draw.h"
-#include "rect.h"
-#include <math.h>
+# include "data.h"
+# include "draw.h"
+# include "rect.h"
+# include <math.h>
 
 # define MINIMAP_OFFSET 30
 # define NB_RAYS SCREEN_WIDTH
 # define FOV_DEG 60
 # define FOV_RAD FOV_DEG * M_PI / 180
 # define PLAYER_ROTATE_SPEED 5
+# define ANGLE_INCREMENT FOV_RAD / (NB_RAYS - 1) // new
 
-typedef struct s_ray //UNFINISHED
+typedef struct s_ray // NEW
 {
 	float		ray_angle;
-	float		angle_increment;
+	float		vertical_ray;
+	float		horizontal_ray;
+	float		ray_length;
+	float		ray_length_correct;
+	int			wall_height;
+	double		wall_x;
+	double		step;
+	int			tex_x;
+	t_img		texture;
 	t_fvector2	side;
 	t_fvector2	delta;
 	t_fvector2	player_center;
@@ -59,9 +68,9 @@ void	draw_minimap(t_data *data, char **map, t_minimap minimap);
 void	update_player(t_data *data, t_player *player);
 void	check_collisions_x(t_game2d *game2d, t_player *player);
 void	check_collisions_y(t_game2d *game2d, t_player *player);
-float	raycasting_up(t_data *data, t_ray ray, float scale);
-float	raycasting_down(t_data *data, t_ray ray, float scale);
-float	raycasting_right(t_data *data, t_ray ray, float scale);
-float	raycasting_left(t_data *data, t_ray ray, float scale);
+float	raycasting_up(t_data *data, t_ray *ray, float scale);
+float	raycasting_down(t_data *data, t_ray *ray, float scale);
+float	raycasting_right(t_data *data, t_ray *ray, float scale);
+float	raycasting_left(t_data *data, t_ray *ray, float scale);
 
 #endif
